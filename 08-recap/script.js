3fe73cccecd579e2dedb379b52738a055b26a795
Pierre Bretéché <pbreteche@dawan.fr>
2020-12-10 11:02:43 +0100

    ajout de produit

--- 08-recap/script.js
+++ 08-recap/script.js
@@ -10,5 +10,24 @@ document.addEventListener('DOMContentLoaded', function(){
     // propriété innerHTML représente le contenu arborescent
     // sous forme de chaine au format HTML
     // permet de définir un sous-arbre éventuellement complexe avec une simple chaine
-    rootElement.innerHTML = maCommande.toHTML();    
+    rootElement.innerHTML = maCommande.toHTML();
+
+    const newProductForm = document.querySelector('#new-product-form');
+    newProductForm.addEventListener('submit', function(event) {
+        // désactiver l'envoi HTTP du formulaire par le navigateur
+        event.preventDefault();
+
+        const newProduct = new Produit(
+            document.querySelector('#new-product-name').value,
+            // opérateur arithmétique unaire "identité" (+), converti la valeur en nombre
+            +document.querySelector('#new-product-price').value,
+        );
+
+        maCommande.ajouter(
+            newProduct,
+            +document.querySelector('#new-product-qte').value
+        );
+
+        console.log(maCommande);
+    });
 });
